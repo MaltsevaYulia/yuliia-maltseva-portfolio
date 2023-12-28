1763c02957e081e237fdbcdee35fb585d207e391
MaltsevaYulia <112495890+MaltsevaYulia@users.noreply.github.com>
2023-12-28 22:53:36 +0200

    update Hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,7 +6,7 @@ import css from "./Hero.module.scss";
 const Hero = () => {
   return (
     <>
-      <section className="bg-[url('/static/png/grid.png')] w-full h-screen bg-cover bg-center flex lg:flex-row flex-col items-center justify-center lg:-mt-20 mt-0">
+      <section className="bg-[url('/static/png/grid.png')] w-full h-screen bg-cover bg-center flex lg:flex-row flex-col items-center lg:-mt-20 mt-0">
         <div className="flex flex-col text-center justify-center mb-2">
           <div className="flex flex-col text-start justify-center mb-2">
             <h1 className={css.text}>Frontend developer</h1>
@@ -26,8 +26,8 @@ const Hero = () => {
           <Image
             src="/assets/hero.jpg"
             alt="Yuliia Maltseva Portrait Photo"
-            width={300}
-            height={300}
+            width={345}
+            height={345}
             priority={true}
             sizes="(100vw, 100vh)"
             className="border-solid border-gray-500 border rounded-xl brightness-125 hover:grayscale transition duration-300 ease-in-out hover:rotate-6 rotate-0"
